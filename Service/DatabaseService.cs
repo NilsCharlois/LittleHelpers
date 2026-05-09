3f5352d85d7579c13a112de99384d51a1b2815f3
nils charlois <nils.charlois@clanwilliamhealth.com>
2026-05-09 15:41:15 +0100

    Small push

--- Service/DatabaseService.cs
+++ Service/DatabaseService.cs
@@ -6,9 +6,12 @@ namespace LittleHelpers.Service
     public class DatabaseService : IDatabaseService
     {
         private Models.AppContext context;
-        public DatabaseService(Models.AppContext context)
+        private IWebHostEnvironment _env;
+
+        public DatabaseService(Models.AppContext context, IWebHostEnvironment env)
         {
             this.context = context;
+            _env = env;
         }
 
         public Task<List<Meal>> GetAllMealsAsync()
@@ -20,5 +23,48 @@ namespace LittleHelpers.Service
         {
             return context.Ingredients.ToListAsync();
         }
+
+        public void Add(Meal meal)
+        {
+            context.Meals.Add(meal);
+            context.SaveChanges();
+        }
+
+        public async Task<Meal> CreateMealAsync(Meal item, Stream fileStream, string extension)
+        {
+            var uploadsFolder = Path.Combine(_env.WebRootPath, "Images");
+
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            var fullPath = Path.Combine(uploadsFolder, fileName);
+
+            using var fs = new FileStream(fullPath, FileMode.Create);
+            await fileStream.CopyToAsync(fs);
+
+            item.ImageUrl = $"/Images/{fileName}";
+
+            context.Meals.Add(item);
+            await context.SaveChangesAsync();
+
+            return item;
+        }
+
+        public void Update(Meal meal)
+        {
+            var existing = context.Meals.FirstOrDefault(x => x.Id == meal.Id);
+            if (existing != null)
+            {
+                existing.IsAirFryer = meal.IsAirFryer;
+                existing.Description = meal.Description;
+                existing.ImageUrl = meal.ImageUrl;
+                existing.Name = meal.Name;
+                existing.MealIngredients = meal.MealIngredients;
+            }
+            context.Entry(existing).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
     }
 }
--- Service/IDatabaseService.cs
+++ Service/IDatabaseService.cs
@@ -4,6 +4,9 @@ namespace LittleHelpers.Service
 {
     public interface IDatabaseService
     {
+        void Add(Meal meal);
+        Task<Meal> CreateMealAsync(Meal item, Stream fileStream, string extension);
+        void Update(Meal meal);
         Task<List<Meal>> GetAllMealsAsync();
         Task<List<Ingredient>> GetAllIngredientsAsync();
     }
